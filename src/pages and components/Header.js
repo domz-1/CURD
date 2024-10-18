baf9d9394fe81d6413e7275572c3a826fc3c3f84
Adham Abo Elenin <adhamelembrator3@gmail.com>
2024-10-18 14:28:23 +0300

    @

--- src/pages and components/Header.js
+++ src/pages and components/Header.js
@@ -1,6 +1,15 @@
 export default function Header(){
     return (
-        <div className="row header">
+        <div className="row header"
+        style={{
+            display: 'flex',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            padding: '10px',
+            gap: '10px',
+            textAlign:"center"
+        }}
+        >
         <h1>Domz Inventory</h1>
         </div>
     )
